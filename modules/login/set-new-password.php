a05e8693d3ca9877b2700e77c498dd4f886c7b3e
Андрей Дубровский <Dronandre@users.noreply.github.com>
2020-05-23 13:49:40 +0700

    Recovery code

--- modules/login/set-new-password.php
+++ modules/login/set-new-password.php
@@ -2,6 +2,39 @@
 
 $pageTitle = "Установить новый пароль";
 
+if ( !empty($_GET['email']) && !empty($_GET['code'])) {
+    $user = R::findOne('users', 'email = ?', array($_GET['email']));
+
+    if (!$user) {
+        header("Location: " . HOST . "lost-password");
+    } 
+} else if (!empty($_POST['set-new-password'])) {
+    $user = R::findOne('users', 'email = ?', array($_POST['email']));
+
+    if($user) {
+        if ($user->recovery_code === $_POST['resetCode'] && $user->recovery_code != '' && $user->recovery_code != NULL) {
+            if(trim($_POST['password'] == '' )){
+                $errors[] = ['title' => 'Введите пароль'];
+            } else if (strlen($_POST['password']) <= 4){
+                $errors[] = ['title' => 'Пароль должен быть больше 4-х символов'];
+            }
+            if( empty($errors) ) {
+                $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $user->recovery_code = '';
+                R::store($user);
+                $success[] = ['title' => 'Пароль успешно обновлен'];
+                $newPasswordReady =- true;
+            }            
+        }
+    } else {
+        $errors[] = ['title' => 'Неверный пароль'];
+    }
+
+} else {
+    header("Location: " . HOST . "lost-password");
+    die();
+}
+
 
 ob_start();
 include ROOT . "templates/login/set-new-password.tpl";
--- templates/login/set-new-password.tpl
+++ templates/login/set-new-password.tpl
@@ -6,12 +6,16 @@
     <?php include ROOT . "templates/components/errors.tpl" ?>
     <?php include ROOT . "templates/components/success.tpl" ?>
 
-    <div class="authorization-form__input">
-        <input name="password" class="input" type="password" placeholder="Новый пароль" />
-    </div>
-    <div class="authorization-form__button">
-        <button name="set-new-password" value="set-new-password" class="primary-button" type="submit">Установить пароль</button>
-    </div>
+    <?php if (!isset($newPasswordReady)) : ?>
+        <div class="authorization-form__input">
+            <input name="password" class="input" type="password" placeholder="Новый пароль" />
+        </div>
+        <input type="hidden" name="email" value="<?php echo $_GET['email'] ?>">
+        <input type="hidden" name="resetCode" value="<?php echo $_GET['code'] ?>">
+        <div class="authorization-form__button">
+            <button name="set-new-password" value="set-new-password" class="primary-button" type="submit">Установить пароль</button>
+        </div>
+    <?php endif; ?>
     <div class="authorization-form__links">
         <a href="<?php echo HOST; ?>login">Вход на сайт</a>
         <a href="<?php echo HOST; ?>registration">Регистрация</a>
